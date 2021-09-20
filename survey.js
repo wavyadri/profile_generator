0f4f62f42a59a0c7217135d8441296b3aad53847
Adriana <adriana.calvomatos@gmail.com>
2021-09-20 00:34:09 +0000

    completed survey

--- survey.js
+++ survey.js
@@ -5,8 +5,26 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('What is your insta handle? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: @${answer}`);
+rl.question('What is your name?', (name) => {
+  rl.question('What is your favourite food?', (food) => {
+    rl.question(
+      'Who is your favourite musical artist or group currently?',
+      (music) => {
+        rl.question('What show/movie do you love to watch?', (film) => {
+          rl.question('What is your instagram handle?', (ig) => {
+            name = name.trim();
+            food = food.trim();
+            music = music.trim();
+            film = film.trim();
+            ig = ig.trim();
 
-  rl.close();
+            const finalAnswer = `If you like ${food}, you and ${name} already have something in common. They can't stop listening to ${music} or watching ${film}. Total classics! Follow for more at @${ig}.`;
+            console.log(finalAnswer);
+
+            rl.close();
+          });
+        });
+      }
+    );
+  });
 });
